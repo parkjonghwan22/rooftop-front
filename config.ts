bceaecc7c54e9556d287d57e8ea53a3816a6d3a3
kimjh <hecticika@gmail.com>
2023-06-19 11:31:35 +0900

    fix configure

--- config.ts
+++ config.ts
@@ -1,8 +1,19 @@
 const config = {
-    PT: 'http',
-    HOST: 'localhost',
+    PT: 'https',
+    HOST: 'www.roof-top.shop',
     PORT: 3000,
     BACKEND_PORT: 3005,
     IMG_PORT: 3005,
 }
 export default config
+
+
+
+// const config = {
+//     PT: 'http',
+//     HOST: '127.0.0.1',
+//     PORT: 3000,
+//     BACKEND_PORT: 3005,
+//     IMG_PORT: 3005,
+// }
+// export default config
