9c8a1a5a7325605a8459d9323515d6103f1862ef
hongtae <ambious12@gmail.com>
2023-06-20 17:28:43 +0900

    feat: pull

--- components/collection/collectioninfo.tsx
+++ components/collection/collectioninfo.tsx
@@ -1,51 +1,67 @@
-import { useEffect, useState } from "react";
-import request from "@utils/request";
-import { SectionWrap, SectionA, Logo, CollectionName, Description, CollectionDatas } from "./styled/collectioninfo.styled";
-import { Mint } from "@components/mint/mint";
+import { useEffect, useState } from 'react'
+import request from '@utils/request'
+import {
+    SectionWrap,
+    SectionA,
+    Logo,
+    CollectionName,
+    Description,
+    CollectionDatas,
+} from './styled/collectioninfo.styled'
+import { Mint } from '@components/mint/mint'
 
 interface Collection {
-    address: string;
-    creator: string;
-    name: string;
-    symbol: string;
-    description: string;
-    url: string;
-    creatorFee: string;
-    logo: string;
+    address: string
+    creator: string
+    name: string
+    symbol: string
+    description: string
+    url: string
+    creatorFee: string
+    logo: string
 }
 
 export const CollectionInfo = ({ address }: { address: string }) => {
-    const [collections, setCollections] = useState<Collection[]>([]);
+    const [collections, setCollections] = useState<Collection[]>([])
 
     const getCollections = async () => {
         try {
-            const { data } = await request.get(`collection/${address}`);
+            const { data } = await request.get(`collection/${address}`)
             // console.log(data);
-            if (data) setCollections(data);
+            if (data) setCollections(data)
         } catch (error) {
-            console.log(error);
+            console.log(error)
         }
-    };
+    }
 
     useEffect(() => {
-        getCollections();
-    }, [address]);
+        getCollections()
+    }, [address])
 
-    if (collections.length === 0) return null;
+    if (collections.length === 0) return null
 
     return (
         <>
             {collections.map((collection) => (
                 <SectionWrap key={collection.address}>
-                    <Logo src={collection.logo} link={collection.url} collectionAddress={collection.address} />
+                    <Logo
+                        src={collection.logo}
+                        link={collection.url}
+                        collectionAddress={collection.address}
+                    />
                     <SectionA>
                         <CollectionName name={collection.name} verified={true} />
                         <Description description={collection.description} />
-                        <CollectionDatas creatorFee={collection.creatorFee} totalVolume={0} floorPrice={0} follows={0} />
+                        <CollectionDatas
+                            creatorFee={collection.creatorFee}
+                            totalVolume={0}
+                            floorPrice={0}
+                            follows={0}
+                        />
                     </SectionA>
                 </SectionWrap>
             ))}
             {/* <Mint collectionAddress={collections[0].address} royalty={collections[0].creatorFee} /> */}
         </>
-    );
-};
+    )
+}
