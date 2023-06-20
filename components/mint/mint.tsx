2de73ff8c4c17b526b45fbf43b9c9fb413dbb65d
hongtae <ambious12@gmail.com>
2023-06-20 16:34:11 +0900

    feat: mint.tsx 수정

--- components/mint/mint.tsx
+++ components/mint/mint.tsx
@@ -1,9 +1,10 @@
 import { ethers } from 'ethers'
-import MarketABI from '@contracts/Marketplace.json';
-import TokenABI from '@contracts/LTToken.json';
-import { useMarket } from '@utils/hooks/useMarket';
-import { useState } from 'react';
-
+import MarketABI from '@contracts/Marketplace.json'
+import TokenABI from '@contracts/LTToken.json'
+import { useMarket } from '@utils/hooks/useMarket'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 interface MintProps {
     collectionAddress: string
@@ -11,27 +12,31 @@ interface MintProps {
 }
 
 export const Mint = ({ collectionAddress, royalty }: MintProps) => {
-    const network = 'http://localhost:8545';
-    const provider = new ethers.JsonRpcProvider(network);
+    const network = 'http://localhost:8545'
+    const provider = new ethers.JsonRpcProvider(network)
     const { market, marketAddress } = useMarket()
     const [latestTokenId, setLatestTokenId] = useState()
-    const [metaData, setMetaData] = useState("")
+    const [metaData, setMetaData] = useState('')
 
-
+    const pending = () => toast.info('Image Uploading...')
+    const success = () => toast.success('Image Uploaded!')
 
     const handleClick = async () => {
-        const signer = await provider.getSigner();
-        const instance = await new ethers.Contract(collectionAddress, TokenABI.abi, signer);
+        const signer = await provider.getSigner()
+        const instance = await new ethers.Contract(collectionAddress, TokenABI.abi, signer)
         console.log(instance)
-        const mintPrice = await instance.mint_price();
-        const account = await signer.address;
+        const mintPrice = await instance.mint_price()
+        const account = await signer.address
 
-        const mintTx = await instance._minting("http://127.0.0.1:3005/profile-picture_1686726038443.gif", {
-            value: mintPrice,
-            from: account,
-        });
+        const mintTx = await instance._minting(
+            'http://127.0.0.1:3005/profile-picture_1686726038443.gif',
+            {
+                value: mintPrice,
+                from: account,
+            }
+        )
         console.log(mintTx)
-        const receipt = await mintTx.wait();
+        const receipt = await mintTx.wait()
         console.log(receipt)
 
         const lastTokenId = await instance.getLatestTokenId()
@@ -41,24 +46,25 @@ export const Mint = ({ collectionAddress, royalty }: MintProps) => {
         const tokenURI = await instance.tokenURI(lastTokenId)
         setMetaData(tokenURI)
 
-
         const ownerOf = await instance.ownerOf(lastTokenId)
         console.log(`ownerOf::`, ownerOf)
         console.log(`mintPrice`, mintPrice)
 
         const approval = await instance.setApprovalForAll(marketAddress, true)
         console.log(`approval::`, approval)
-
-
     }
 
     const handleClick2 = async () => {
         try {
-
-
             // const gasPrice = ethers.parseUnits('20000', 'gwei');
-            const creatorFee = parseFloat(royalty.replace("%", "")) * 10;
-            const addOnMarket = await market.addNftToMarket(collectionAddress, latestTokenId, 4, metaData, creatorFee)
+            const creatorFee = parseFloat(royalty.replace('%', '')) * 10
+            const addOnMarket = await market.addNftToMarket(
+                collectionAddress,
+                latestTokenId,
+                4,
+                metaData,
+                creatorFee
+            )
             console.log(`addOnMarket::`, addOnMarket)
 
             const getNFTs = await market.getAllTokensInCollection(collectionAddress)
@@ -74,17 +80,15 @@ export const Mint = ({ collectionAddress, royalty }: MintProps) => {
 
             const testMarket1 = await market.marketAddress()
             console.log(`test1::`, testMarket1)
-        } catch (e) { console.log(e) }
+        } catch (e) {
+            console.log(e)
+        }
     }
 
     return (
         <>
-            <button onClick={handleClick}>
-                minting
-            </button>
-            <button onClick={handleClick2}>
-                handler2
-            </button>
+            <button onClick={handleClick}>minting</button>
+            <button onClick={handleClick2}>handler2</button>
         </>
     )
 }
