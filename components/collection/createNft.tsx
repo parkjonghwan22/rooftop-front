46fc630d56b356bf2930ecb89e8081dbfbc64327
hongtae <ambious12@gmail.com>
2023-06-21 15:12:43 +0900

    feat: createNFT , createMetaData . componenet 만들기 전

--- components/collection/createNft.tsx
+++ components/collection/createNft.tsx
@@ -1,22 +1,39 @@
 import { useInput } from '@utils/hooks/useInput'
 import { CreateNftWrapper, NftFormContainer, NftTitle, NftLabel } from './styled/createNft.styled'
 import request from '@utils/request'
-import { useState } from 'react'
-import { FileInputBox, InputBox, TextArea } from '@components/common/input'
+import React, { useState } from 'react'
+import { InputBox, TextArea } from '@components/common/input'
 import { Button } from '@components/common/button'
 import { LoadingSpinner } from '@components/common/loading/loading'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { FileNftInputBox } from '@components/common/input/fileNftInputBox'
+import { useMint } from '@utils/hooks/useMint'
+
+interface MintProps {
+    collectionAddress: string
+    royalty: string
+}
+
 interface CreateNftProps {
     setIsOpenModal: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export const CreateNft = ({ setIsOpenModal }: CreateNftProps) => {
+export const CreateNft = ({
+    setIsOpenModal,
+    collectionAddress,
+    royalty,
+}: CreateNftProps & MintProps) => {
     const [isFocused, setIsFocused] = useState('')
     const [isDuplicated, setIsDuplicated] = useState(false)
     const [isLoading, setIsLoading] = useState(false)
     const [nftImage, setNftImage] = useState('')
+
     const nftName = useInput('')
     const nftPrice = useInput('')
     const nftDescription = useInput('')
+
+    const success = () => toast.success('Image Uploaded!')
 
     const handleInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
         try {
@@ -31,10 +48,49 @@ export const CreateNft = ({ setIsOpenModal }: CreateNftProps) => {
             console.log(error)
         }
     }
+
+    const handlePinataSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        setIsLoading(true)
+        if (nftImage) {
+            setIsLoading(false)
+            console.log('nftImage IPFSHASH', nftImage)
+        }
+
+        try {
+            if (isLoading) return
+
+            fetch('/api/verify', {
+                method: 'POST',
+                body: JSON.stringify({
+                    name: nftName.value,
+                    description: nftDescription.value,
+                    image: nftImage,
+                }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log('mint3 resData', data)
+                    const tokenURI = `ipfs://${data.IpfsHash}`
+                    const { newToken } = useMint({
+                        tokenURI,
+                        collectionAddress,
+                        royalty,
+                        tokenPrice: nftPrice.value as string | number,
+                    })
+                    console.log('new Token : ', newToken)
+                })
+                .catch((error) => console.log(error))
+        } catch (e: any) {
+            console.log(e.message)
+        }
+    }
+
     return (
         <>
             <CreateNftWrapper>
-                <NftFormContainer>
+                <NftFormContainer onSubmit={handlePinataSubmit}>
                     <NftTitle className="text-center">
                         Welcome! This is where you register your NFT.
                     </NftTitle>
@@ -66,14 +122,20 @@ export const CreateNft = ({ setIsOpenModal }: CreateNftProps) => {
                         placeholder="Please introduce your NFT"
                     />
                     <NftLabel htmlFor="NftImage">대표 이미지 등록</NftLabel>
-                    <FileInputBox state={nftImage} setState={setNftImage} />
+                    <FileNftInputBox
+                        state={nftImage}
+                        setState={setNftImage}
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                    />
                     {isLoading ? (
                         <Button type="submit" color="blue" disabled>
-                            <LoadingSpinner /> 등록중...
+                            <LoadingSpinner /> Uploading...
                         </Button>
                     ) : (
                         <Button type="submit" color="blue">
-                            컬렉션 등록
+                            NFT Registration
                         </Button>
                     )}
                 </NftFormContainer>
