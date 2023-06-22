2e771f669154a78948f0b3841a978a3aed73956d
parkjonghwan22 <gch02933@naver.com>
2023-06-23 07:07:32 +0900

    feat: Myprofile 작업

--- components/profile/profile.tsx
+++ components/profile/profile.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-import { UserType } from "@utils/types/user.interface"
-import { ProfileCardWrap, UserAddress } from "./styled/Profile.styled"
-import { Icon } from '@iconify/react';
+import Image from "next/image";
+import { UserType } from "@utils/types/user.interface";
+import { ProfileCardWrap, UserAddress } from "./styled/Profile.styled";
+import { Icon } from "@iconify/react";
 import { Button } from "@components/common/button";
 import { useState } from "react";
 import { useInput } from "@utils/hooks/useInput";
@@ -9,71 +9,139 @@ import { InputBox } from "@components/common/input";
 import { FileInputCircle } from "@components/common/input/fileinputcircle";
 import request from "@utils/request";
 import { Alert } from "@components/common/alert";
-
-
+import { Mynft } from "./styled/Mynfts.styled";
+import { Activity } from "./styled/Activity.styled";
+import { Favorites } from "./styled/Favorites.styled";
 
 export const ProfileCard = ({ user }: { user: UserType | null }) => {
-    const [isOpenAlert, setIsOpenAlert] = useState(false)
-    const [modify, setModify] = useState(false)
-    const [userImg, setUserImg] = useState<string>("")
-    const username = useInput(user?.name);
-    const slicedAddress = user!.address.slice(0, 6) + "..." + user!.address.slice(-4);
+  const [isOpenAlert, setIsOpenAlert] = useState(false);
+  const [modify, setModify] = useState(false);
+  const [userImg, setUserImg] = useState<string>("");
 
-    const handleUpdate = async () => {
-        setModify(prevState => !prevState);
+  const [activeTab, setActiveTab] = useState("nfts");
 
-        if (modify) {
-            const res = await request.put('auth/update', {
-                address: user!.address,
-                name: username.value,
-                userImg: userImg
-            })
-            if (res.status === 200) {
-                window.location.reload();
-            }
-        }
-    };
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+  };
 
-    const handleCopy = () => {
-        navigator.clipboard.writeText(user!.address);
-        setIsOpenAlert(true)
+  const username = useInput(user?.name);
+  const slicedAddress =
+    user!.address.slice(0, 6) + "..." + user!.address.slice(-4);
+
+  const handleUpdate = async () => {
+    setModify((prevState) => !prevState);
+
+    if (modify) {
+      const res = await request.put("auth/update", {
+        address: user!.address,
+        name: username.value,
+        userImg: userImg,
+      });
+      if (res.status === 200) {
+        window.location.reload();
+      }
     }
+  };
 
+  const handleCopy = () => {
+    navigator.clipboard.writeText(user!.address);
+    setIsOpenAlert(true);
+  };
 
-    if (!user) return <></>
-    return (
-        <>
-            <ProfileCardWrap>
-                <div className="absolute top-5 right-5 w-28">
-                    <Button {...(!modify) ? { color: "blue" } : { color: "green" }} fontSize="sm" onClick={handleUpdate}>
-                        <Icon icon="bxs:edit" className="mr-1 text-lg" />{(!modify) ? "Edit" : "Confirm"}
-                    </Button>     
-                </div>      
-                <div className="flex justify-center">
-                    <div className="rounded-full mx-auto absolute -top-20 w-32 h-32 shadow-md border-4 border-white transition duration-200 transform hover:scale-110">
-                        {(!modify)
-                        ? <Image src={user.userImg} alt="user" width={200} height={200} className="w-full h-full rounded-full" />
-                        : <FileInputCircle state={userImg} setState={setUserImg} />}
-                    </div>
-                </div>
-                <div className="mt-16">
-                    {(!modify)
-                        ? <h1 className="font-bold text-center text-3xl">{user.name}</h1>
-                        : <div className="flex items-center justify-center">
-                            <InputBox value={username.value} onChange={username.onChange} fontSize="xl" />
-                        </div>}
+  if (!user) return <></>;
+  return (
+    <>
+      <ProfileCardWrap>
+        <div className="absolute top-5 right-5 w-28">
+          <Button
+            {...(!modify ? { color: "blue" } : { color: "green" })}
+            fontSize="sm"
+            onClick={handleUpdate}
+          >
+            <Icon icon="bxs:edit" className="mr-1 text-lg" />
+            {!modify ? "Edit" : "Confirm"}
+          </Button>
+        </div>
+        <div className="flex justify-center">
+          <div className="rounded-full mx-auto absolute -top-20 w-32 h-32 shadow-md border-4 border-white transition duration-200 transform hover:scale-110">
+            {!modify ? (
+              <Image
+                src={user.userImg}
+                alt="user"
+                width={200}
+                height={200}
+                className="w-full h-full rounded-full"
+              />
+            ) : (
+              <FileInputCircle state={userImg} setState={setUserImg} />
+            )}
+          </div>
+        </div>
+        <div className="mt-16">
+          {!modify ? (
+            <h1 className="font-bold text-center text-3xl">{user.name}</h1>
+          ) : (
+            <div className="flex items-center justify-center">
+              <InputBox
+                value={username.value}
+                onChange={username.onChange}
+                fontSize="xl"
+              />
+            </div>
+          )}
 
-                    <div className="my-5 px-6">
-                        <UserAddress onClick={handleCopy}>{slicedAddress}<Icon icon="bxs:copy" className="ml-1" /></UserAddress>
-                    </div>
-                    <div className="flex justify-between items-center my-5 px-6">
-                        <a href="" className="text-gray-500 hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white rounded transition duration-150 ease-in font-medium text-sm text-center w-full py-3">My NFTs</a>
-                        <a href="" className="text-gray-500 hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white rounded transition duration-150 ease-in font-medium text-sm text-center w-full py-3">Activity</a>
-                        <a href="" className="text-gray-500 hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white rounded transition duration-150 ease-in font-medium text-sm text-center w-full py-3">Favorites</a>
-                    </div>
-                </div>
-            </ProfileCardWrap>
-            <Alert isOpenAlert={isOpenAlert} setIsOpenAlert={setIsOpenAlert} color="green">지갑 주소가 복사되었습니다</Alert>
-        </>
-    )
-}
+          <div className="my-5 px-6">
+            <UserAddress onClick={handleCopy}>
+              {slicedAddress}
+              <Icon icon="bxs:copy" className="ml-1" />
+            </UserAddress>
+          </div>
+          <div className="flex justify-between items-center my-5 px-6">
+            <div
+              className={`text-gray-500 hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white rounded transition duration-150 ease-in font-medium text-sm text-center w-full py-3 cursor-pointer ${
+                activeTab === "nfts"
+                  ? "bg-gray-100 dark:bg-gray-600 text-gray-900"
+                  : ""
+              }`}
+              onClick={() => handleTabClick("nfts")}
+            >
+              My NFTs
+            </div>
+            <div
+              className={`text-gray-500 hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white rounded transition duration-150 ease-in font-medium text-sm text-center w-full py-3 cursor-pointer ${
+                activeTab === "activity"
+                  ? "bg-gray-100 dark:bg-gray-600 text-gray-900"
+                  : ""
+              }`}
+              onClick={() => handleTabClick("activity")}
+            >
+              Activity
+            </div>
+            <div
+              className={`text-gray-500 hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white rounded transition duration-150 ease-in font-medium text-sm text-center w-full py-3 cursor-pointer ${
+                activeTab === "favorites"
+                  ? "bg-gray-100 dark:bg-gray-600 text-gray-900"
+                  : ""
+              }`}
+              onClick={() => handleTabClick("favorites")}
+            >
+              Favorites
+            </div>
+          </div>
+        </div>
+      </ProfileCardWrap>
+      <div className="mt-16 mx-auto md:w-5/6 bg-gray-700 dark:bg-gray-800 rounded-lg">
+        {activeTab === "nfts" && <Mynft />}
+        {activeTab === "activity" && <Activity />}
+        {activeTab === "favorites" && <Favorites />}
+      </div>
+      <Alert
+        isOpenAlert={isOpenAlert}
+        setIsOpenAlert={setIsOpenAlert}
+        color="green"
+      >
+        지갑 주소가 복사되었습니다
+      </Alert>
+    </>
+  );
+};
---
+++ components/profile/styled/Mynfts.styled.tsx
@@ -0,0 +1,225 @@
+import tw from "tailwind-styled-components";
+import Image from "next/image";
+
+export const MynftsWrap = tw.div`
+     rounded-lg mx-auto w-full flex flex-col items-center
+`;
+
+export const Mynft = () => {
+  return (
+    <>
+      <MynftsWrap>
+        <div className="flex flex-wrap justify-center mt-2">
+          <div className="w-72 hover:transform hover:scale-105 transition duration-300 cursor-pointer mx-2 my-3">
+            <div>
+              <Image
+                src="http://localhost:3000/test2.png"
+                alt="test"
+                width={1000}
+                height={1000}
+                className="object-fill w-72 h-60 mx-auto rounded-t-lg"
+              />
+            </div>
+            <div className="rounded-b-lg w-72 h-32 mx-auto bg-white dark:bg-gray-900 shadow-lg opacity-90 ">
+              <div className="text-xl text-center pt-4 pb-4 font-bold">
+                Missed Yet Anthor..
+              </div>
+              <div className="flex flex-wrap pl-5">
+                <div className="w-1/2">
+                  <div className="text-gray-400">Floor</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+                <div className="w-1/2">
+                  <div className="text-gray-400">Total Volume</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-72 hover:transform hover:scale-105 transition duration-300 cursor-pointer mx-2 my-3">
+            <div>
+              <Image
+                src="http://localhost:3000/test3.png"
+                alt="test"
+                width={1000}
+                height={1000}
+                className="object-fill w-72 h-60 mx-auto rounded-t-lg"
+              />
+            </div>
+            <div className="rounded-b-lg w-72 h-32 mx-auto bg-white dark:bg-gray-900 shadow-lg opacity-90 ">
+              <div className="text-xl text-center pt-4 pb-4 font-bold">
+                Missed Yet Anthor..
+              </div>
+              <div className="flex flex-wrap pl-5">
+                <div className="w-1/2">
+                  <div className="text-gray-400">Floor</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+                <div className="w-1/2">
+                  <div className="text-gray-400">Total Volume</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-72 hover:transform hover:scale-105 transition duration-300 cursor-pointer mx-2 my-3">
+            <div>
+              <Image
+                src="http://localhost:3000/test6.png"
+                alt="test"
+                width={1000}
+                height={1000}
+                className="object-fill w-72 h-60 mx-auto rounded-t-lg"
+              />
+            </div>
+            <div className="rounded-b-lg w-72 h-32 mx-auto bg-white dark:bg-gray-900 shadow-lg opacity-90 ">
+              <div className="text-xl text-center pt-4 pb-4 font-bold">
+                Missed Yet Anthor..
+              </div>
+              <div className="flex flex-wrap pl-5">
+                <div className="w-1/2">
+                  <div className="text-gray-400">Floor</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+                <div className="w-1/2">
+                  <div className="text-gray-400">Total Volume</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-72 hover:transform hover:scale-105 transition duration-300 cursor-pointer mx-2 my-3">
+            <div>
+              <Image
+                src="http://localhost:3000/test8.png"
+                alt="test"
+                width={1000}
+                height={1000}
+                className="object-fill w-72 h-60 mx-auto rounded-t-lg"
+              />
+            </div>
+            <div className="rounded-b-lg w-72 h-32 mx-auto bg-white dark:bg-gray-900 shadow-lg opacity-90 ">
+              <div className="text-xl text-center pt-4 pb-4 font-bold">
+                Missed Yet Anthor..
+              </div>
+              <div className="flex flex-wrap pl-5">
+                <div className="w-1/2">
+                  <div className="text-gray-400">Floor</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+                <div className="w-1/2">
+                  <div className="text-gray-400">Total Volume</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-72 hover:transform hover:scale-105 transition duration-300 cursor-pointer mx-2 my-3">
+            <div>
+              <Image
+                src="http://localhost:3000/test5.png"
+                alt="test"
+                width={1000}
+                height={1000}
+                className="object-fill w-72 h-60 mx-auto rounded-t-lg"
+              />
+            </div>
+            <div className="rounded-b-lg w-72 h-32 mx-auto bg-white dark:bg-gray-900 shadow-lg opacity-90 ">
+              <div className="text-xl text-center pt-4 pb-4 font-bold">
+                Missed Yet Anthor..
+              </div>
+              <div className="flex flex-wrap pl-5">
+                <div className="w-1/2">
+                  <div className="text-gray-400">Floor</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+                <div className="w-1/2">
+                  <div className="text-gray-400">Total Volume</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-72 hover:transform hover:scale-105 transition duration-300 cursor-pointer mx-2 my-3">
+            <div>
+              <Image
+                src="http://localhost:3000/test2.png"
+                alt="test"
+                width={1000}
+                height={1000}
+                className="object-fill w-72 h-60 mx-auto rounded-t-lg"
+              />
+            </div>
+            <div className="rounded-b-lg w-72 h-32 mx-auto bg-white dark:bg-gray-900 shadow-lg opacity-90 ">
+              <div className="text-xl text-center pt-4 pb-4 font-bold">
+                Missed Yet Anthor..
+              </div>
+              <div className="flex flex-wrap pl-5">
+                <div className="w-1/2">
+                  <div className="text-gray-400">Floor</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+                <div className="w-1/2">
+                  <div className="text-gray-400">Total Volume</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-72 hover:transform hover:scale-105 transition duration-300 cursor-pointer mx-2 my-3">
+            <div>
+              <Image
+                src="http://localhost:3000/test6.png"
+                alt="test"
+                width={1000}
+                height={1000}
+                className="object-fill w-72 h-60 mx-auto rounded-t-lg"
+              />
+            </div>
+            <div className="rounded-b-lg w-72 h-32 mx-auto bg-white dark:bg-gray-900 shadow-lg opacity-90 ">
+              <div className="text-xl text-center pt-4 pb-4 font-bold">
+                Missed Yet Anthor..
+              </div>
+              <div className="flex flex-wrap pl-5">
+                <div className="w-1/2">
+                  <div className="text-gray-400">Floor</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+                <div className="w-1/2">
+                  <div className="text-gray-400">Total Volume</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-72 hover:transform hover:scale-105 transition duration-300 cursor-pointer mx-2 my-3">
+            <div>
+              <Image
+                src="http://localhost:3000/test7.png"
+                alt="test"
+                width={1000}
+                height={1000}
+                className="object-fill w-72 h-60 mx-auto rounded-t-lg"
+              />
+            </div>
+            <div className="rounded-b-lg w-72 h-32 mx-auto bg-white dark:bg-gray-900 shadow-lg opacity-90 ">
+              <div className="text-xl text-center pt-4 pb-4 font-bold">
+                Missed Yet Anthor..
+              </div>
+              <div className="flex flex-wrap pl-5">
+                <div className="w-1/2">
+                  <div className="text-gray-400">Floor</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+                <div className="w-1/2">
+                  <div className="text-gray-400">Total Volume</div>
+                  <div className="font-bold">0.03 ETH</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </MynftsWrap>
+    </>
+  );
+};
