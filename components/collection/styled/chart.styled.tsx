881de7e491694ad2aaa7abf9f7f0de8ec259e0af
hongtae <ambious12@gmail.com>
2023-06-28 16:30:28 +0900

    fix : commit

---
+++ components/collection/styled/chart.styled.tsx
@@ -0,0 +1,36 @@
+// import { Line } from 'react-chartjs-2';
+// import { ChartData, ChartOptions } from 'chart.js';
+
+// const chartData: ChartData = {
+//     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//     datasets: [
+//       {
+//         label: 'My First dataset',
+//         data: [65, 59, 80, 81, 56, 55, 40],
+//         fill: false,
+//         backgroundColor: 'rgba(75, 192, 192, 0.4)',
+//         borderColor: 'rgba(75, 192, 192, 1)',
+//       },
+//     ],
+//   };
+  
+//   const chartOptions: ChartOptions = {
+//     responsive: true,
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//       },
+//     },
+//   };
+
+//   const Chart = () => {
+//     return (
+//       <div>
+//         <h2>Line Example</h2>
+//         <Line data={chartData} options={chartOptions} />
+//       </div>
+//     );
+//   };
+  
+//   export default Chart;
+  
