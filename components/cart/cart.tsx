1359316ef2d0150d53376c2606982bdf9f9c7af7
kimjh <hecticika@gmail.com>
2023-06-29 12:47:49 +0900

    fix merge

--- components/cart/cart.tsx
+++ components/cart/cart.tsx
@@ -8,21 +8,36 @@ import { Icon } from "@iconify/react";
 
 
 const CartItem = () => {
+
+
+  // const handleDeleteCartItem = async (id) => {
+  //   if (!address) return
+
+  //   try {
+  //     await request.delete(`cart/${address}/${id}`);
+  //     console.log("장바구니 아이템이 삭제되었습니다.");
+  //   } catch (error) {
+  //     console.error("error :", error);
+  //   }
+  // };
+
   return (
     <li className="flex py-6">
       <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
         <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg" alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." className="h-full w-full object-cover object-center" />
       </div>
-
       <div className="ml-4 flex flex-1 flex-col">
         <div>
-          <div className="flex justify-between text-base font-medium text-gray-900">
+          <div className="flex justify-between text-base font-medium text-gray-900 dark:text-gray-100">
             <h3>
               <a href="#">Throwback Hip Bag</a>
             </h3>
+            <div>
+            <p className="ml-4">100 MATIC</p>
             <p className="ml-4">$90.00</p>
+            </div>
           </div>
-          <p className="mt-1 text-sm text-gray-500">Salmon</p>
+          <p className="mt-1 text-sm text-gray-500 dark:text-gray-300">Salmon</p>
         </div>
         <div className="flex flex-1 items-end justify-between text-sm">
           <p className="text-gray-500">Qty 1</p>
@@ -33,9 +48,7 @@ const CartItem = () => {
         </div>
       </div>
     </li>
-
   )
-
 }
 
 
@@ -50,58 +63,42 @@ const Cart = ({ cartData, setIsOpenCart }: CartProps) => {
 
 
 
-  // const handleDeleteCartItem = async (id) => {
-  //   if (!address) return
-
-  //   try {
-  //     await request.delete(`cart/${address}/${id}`);
-  //     console.log("장바구니 아이템이 삭제되었습니다.");
-  //   } catch (error) {
-  //     console.error("error :", error);
-  //   }
-  // };
-
-
   return (
-    <div className="relative z-10">
-        <div className="absolute inset-0 overflow-hidden">
-          <div className="fixed inset-y-0 right-0 flex max-w-full pl-10">
-            <div className="w-screen max-w-md">
-              <div className="flex h-full flex-col overflow-y-scroll bg-white dark:bg-gray-900 shadow-xl">
-                <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
-                  <div className="flex items-start justify-between">
-                    <h2 className="text-lg font-medium text-gray-900 dark:text-gray-100" id="slide-over-title">My Cart</h2>
-                    <div className="ml-3 flex h-7 items-center">
-                      <button type="button" onClick={() => setIsOpenCart(false)} className="-m-2 p-2 text-gray-400 dark:text-gray-200 hover:text-gray-500 dark:hover:text-white">
-                        <Icon icon="ph:x" className="ml-auto text-xl cursor-pointer" />
-                      </button>
-                    </div>
-                  </div>
-                  <div className="mt-8">
-                    <div className="flow-root">
-                      <ul role="list" className="-my-6 divide-y divide-gray-200">
-                        <CartItem />
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-                <div className="border-t border-gray-200 dark:border-gray-500 px-4 py-6 sm:px-6">
-                  <div className="flex justify-between text-base font-medium text-gray-900 dark:text-gray-100">
-                    <p>Total Price</p>
-                    <div>
-                      <p>20 MATIC</p>
-                      <p className="text-green-500">$262.00</p>
-                    </div>
-                  </div>
-                  <div className="my-6">
-                    <a href="#" className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Complete purchase</a>
-                  </div>
-                </div>
+    <div className="z-10 fixed inset-y-0 right-0 flex max-w-full pl-10">
+      <div className="w-screen max-w-md">
+        <div className="flex h-full flex-col overflow-y-scroll bg-white dark:bg-gray-900 shadow-xl  animate-slideLeft">
+          <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+            <div className="flex items-start justify-between">
+              <h2 className="text-lg font-medium text-gray-900 dark:text-gray-100" id="slide-over-title">My Cart</h2>
+              <div className="ml-3 flex h-7 items-center">
+                <button type="button" onClick={() => setIsOpenCart(false)} className="-m-2 p-2 text-gray-400 dark:text-gray-200 hover:text-gray-500 dark:hover:text-white">
+                  <Icon icon="ph:x" className="ml-auto text-xl cursor-pointer" />
+                </button>
               </div>
+            </div>
+            <div className="mt-8">
+              <div className="flow-root">
+                <ul role="list" className="-my-6 divide-y divide-gray-200">
+                  {cartData ? <CartItem /> : <span>Empty</span>}
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="border-t border-gray-200 dark:border-gray-500 px-4 py-6 sm:px-6">
+            <div className="flex justify-between text-base font-medium text-gray-900 dark:text-gray-100">
+              <p>Total Price</p>
+              <div>
+                <p>20 MATIC</p>
+                <p className="text-green-500">$262.00</p>
+              </div>
+            </div>
+            <div className="my-6">
+              <a href="#" className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Complete purchase</a>
             </div>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
