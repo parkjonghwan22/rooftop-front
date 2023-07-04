afffdb47050dbcb83e5e5ad885c575ef59b07cdb
hongtae <ambious12@gmail.com>
2023-07-04 16:31:50 +0900

    feat: 오타수정

--- pages/rankings.tsx
+++ pages/rankings.tsx
@@ -1,3 +1,4 @@
+import { LoadingSpinner2 } from '@components/common/loading'
 import { RootLayout } from '@components/layout/layout'
 import Stats from '@components/stats/stats'
 import request from '@utils/request'
@@ -21,7 +22,7 @@ const Rankings = () => {
 
     const isLoading = getAllCollectionLoading
 
-    if (isLoading || !allCollectionData) return <p>Loading...</p> // 로딩 컴포넌트쫌!!!
+    if (isLoading || !allCollectionData) return <LoadingSpinner2/>
 
     return (
         <RootLayout>
