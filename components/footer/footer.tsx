828cf95a2b6b6774ed67d71b8778fa4627dd8ac9
kimjh <hecticika@gmail.com>
2023-07-07 11:35:08 +0900

    fix logo

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -11,18 +11,7 @@ const Footer = () => {
         <footer className="bg-gray-100 dark:bg-gray-800 mt-20">
             <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
                 <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        className="w-10 h-10 text-white p-2 bg-red-500 rounded-full"
-                        viewBox="0 0 24 24"
-                    >
-                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                    </svg>
+                    <Icon icon="cryptocurrency:matic" className="text-3xl text-red-500" />
                     <span className="ml-3 text-xl dark:text-white">ROOFTOP</span>
                 </a>
                 <p className="text-md text-gray-500 sm:ml-6 sm:mt-0 mt-4">
--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -2,8 +2,8 @@ import Footer from "@components/footer/footer";
 import Header from "@components/header/header";
 
 const ContentLayout = ({ children }: { children: React.ReactNode }) => {
-    return <div className="pt-40">{children}</div>;
-    // return <div className="pt-40 from-transparent to-[#999] dark:to-[#] bg-cover bg-no-repeat before:absolute before:left-0 before:top-0 before:h-[320px] before:w-full before:bg-gradient-to-t">{children}</div>;
+    // return <div className="pt-40">{children}</div>;
+    return <div className="pt-40 from-transparent to-[#999] dark:to-[#] bg-cover bg-no-repeat before:absolute before:left-0 before:top-0 before:h-[260px] before:w-full before:bg-gradient-to-t">{children}</div>;
 };
 
 export const RootLayout = ({ children }: { children: React.ReactNode }) => {
