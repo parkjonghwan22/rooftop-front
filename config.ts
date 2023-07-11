71c506de99172a963f3a208db8b5e249a23e749c
kimjh <hecticika@gmail.com>
2023-07-11 11:45:59 +0900

    fix next image

--- config.ts
+++ config.ts
@@ -1,15 +1,15 @@
-// const config = {
-//   APP_URL: "http://localhost:3000",
-//   API_URL: "http://localhost:3005",
-//   PINATA_API_KEY: process.env.PINATA_API_KEY,
-//   PINATA_SECRET_API_KEY: process.env.PINATA_SECRET_API_KEY,
-// };
-// export default config;
-
 const config = {
-  APP_URL: "https://roof-top.shop/",
-  API_URL: "https://nest-deploy-c764d61cc1b8.herokuapp.com",
+  APP_URL: "http://localhost:3000",
+  API_URL: "http://localhost:3005",
   PINATA_API_KEY: process.env.PINATA_API_KEY,
   PINATA_SECRET_API_KEY: process.env.PINATA_SECRET_API_KEY,
 };
 export default config;
+
+// const config = {
+//   APP_URL: "https://roof-top.shop/",
+//   API_URL: "https://nest-deploy-c764d61cc1b8.herokuapp.com",
+//   PINATA_API_KEY: process.env.PINATA_API_KEY,
+//   PINATA_SECRET_API_KEY: process.env.PINATA_SECRET_API_KEY,
+// };
+// export default config;
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['127.0.0.1', 'localhost', 'roof-top.shop', 'i.seadn.io', 'ipfs.io', 'dummyimage.com'],
+    domains: ['127.0.0.1', 'localhost', 'roof-top.shop', 'nest-deploy-c764d61cc1b8.herokuapp.com', 'i.seadn.io', 'ipfs.io', 'dummyimage.com'],
   },
 };
 
